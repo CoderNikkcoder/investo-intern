ea4ec69ebb38181430bb900e23f70e078ebad9d3
CoderNikkcoder <159905764+CoderNikkcoder@users.noreply.github.com>
2024-03-10 19:57:48 +0530

    Create unit_test.py

---
+++ test/unit_test.py
@@ -0,0 +1,21 @@
+import unittest
+import pandas as pd
+import datetime
+
+class TestTickData(unittest.TestCase):
+    def test_values(self):
+       
+        df = pd.read_excel('C:\\Users\\HP\\OneDrive\\Desktop\\AssHINDALCO_1D.xlsx')
+
+        for index, row in df.iterrows():
+            
+            self.assertIsInstance(row['datetime'], datetime.datetime)
+           
+            for field in ['close', 'high', 'low', 'open']:
+                self.assertIsInstance(row[field], float)
+
+            self.assertIsInstance(row['volume'], int)
+            self.assertIsInstance(row['instrument'], str)
+
+if __name__ == '__main__':
+    unittest.main()
